f88086095c7b0879a15b9b3ac4367cb564593cca
RoastedBrotato <waleedajaz1996@gmail.com>
2025-01-07 11:48:23 +0300

    run test

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,23 @@ use std::path::PathBuf;
 use std::process::Command;
 fn find_exe(name: &str) -> Option<PathBuf> {
     if let Ok(paths) = env::var("PATH") {
-Expand 20 lines
+        for path in env::split_paths(&paths) {
+            let exe_path = path.join(name);
+            if exe_path.is_file() {
+                return Some(exe_path);
+            }
+        }
+    }
+    None
+}
+fn main() {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
         let cmds: Vec<_> = input.split_whitespace().collect();
         if cmds.is_empty() {
             return;
