1e4d49c26dea919868fd26036ee791ee180d1bed
RoastedBrotato <waleedajaz1996@gmail.com>
2025-01-07 09:43:41 +0300

    type test

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,19 @@ fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
+
         let stdin = io::stdin();
         let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
-        let trimmed_input = input.trim();
-        let command = trimmed_input.split_whitespace().next().unwrap();
-        // let args = trimmed_input.split_whitespace().collect::<Vec<&str>>();
-        match command {
-            "echo" => println!("{}", trimmed_input.replacen("echo ", "", 1)),
-            "exit" => break,
-            _ => println!("{}: command not found", trimmed_input),
+        stdin.read_line(&mut input)?;
+        let tokens: Vec<&str> = input.trim().split(' ').filter(|s| !s.is_empty()).collect();
+        match tokens[..] {
+            ["exit", code] => std::process::exit(code.parse::<u8>().unwrap().into()),
+            ["echo", ..] => println!("{}", tokens[1..].join(" ")),
+            ["type", cmd] => match cmd {
+                "exit" | "echo" | "type" => println!("{cmd} is a shell builtin"),
+                _ => println!("{cmd}: not found"),
+            },
+            _ => println!("{}: command not found", input.trim()),
         }
     }
 }
